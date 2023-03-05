363b6b3b824a8c14b551fa1833333a79aac1cf55
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-05 16:03:04 -0700

    commit tree 60947b920192606c734798dc6b7aba78ab61eed6

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,17 @@
 class Tree
+
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+    until nodes_to_visit.empty?
+      node = nodes_to_visit.pop
+      return node if node[:id] == id
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+    nil
+  end
+
 end
